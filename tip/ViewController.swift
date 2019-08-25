e2c1c865f7189bf230d22c353f3541c325ab241e
Abdallah Chidjou <salamiolatoundji@gmail.com>
2019-08-25 17:34:47 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Abdison Salami on 8/25/19.
+//  Copyright © 2019 Abdison Salami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
